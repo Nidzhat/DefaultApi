fba3bb16b984a98b22892ba231f7269ecb1b702a
Ниджат Туленов <xiromant.dub@mai.ru>
2021-05-19 04:22:10 +0600

    Добавьте файлы проекта.

---
+++ DefaultApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DefaultApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DefaultApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private static List<WeatherInfo> weatherInfoList =
+            new List<WeatherInfo>
+            {
+                new WeatherInfo {
+                    Id = 1,
+                    city = "Astana",
+                    date = DateTime.Today,
+                    maxTemp = 38,
+                    minTemp = 35,
+                    WeatherType = WeatherTypes.Cloudy
+                },
+                new WeatherInfo {
+                    Id = 2,
+                    city = "Almaty",
+                    date = DateTime.Today.AddDays(1),
+                    maxTemp = 26,
+                    minTemp = 15,
+                    WeatherType = WeatherTypes.Rain
+                }
+
+            };
+
+        [HttpGet]
+        public IEnumerable<WeatherInfo> Get()
+        {
+            return weatherInfoList;
+        }
+
+        [HttpGet("{Id}")]
+        public WeatherInfo Get(int Id) {
+            return weatherInfoList.FirstOrDefault(wi => wi.Id == Id);
+        }
+
+    }
+}
---
+++ DefaultApi/Models/WeatherInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefaultApi.Models
+{
+    public class WeatherInfo
+    {
+        public int Id { get; set; }
+        public int maxTemp { get; set; }
+        public int minTemp { get; set; }
+        public string city { get; set; }
+        public DateTime date { get; set; }
+
+        public WeatherTypes WeatherType { get; set; }
+    }
+}
